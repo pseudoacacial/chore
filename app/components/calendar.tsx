63d2e32ad4404bed2ea662eb5c9c18e91b6cd6b9
pseudoacacial <aibanzai@protonmail.com>
2026-02-02 23:00:31 +0100

    start of the week

--- app/components/calendar.tsx
+++ app/components/calendar.tsx
@@ -7,21 +7,41 @@ type CalendarProps = {
   date: Date;
 };
 
+// magic number from config
+// 0 means sunday, 1 means monday
+const weekStartDay = 1;
+
+const getPreviousStartOfTheWeek = (date: Date, startOfTheWeek = 0) => {
+  if (date.getDay() === startOfTheWeek) return date;
+  const prevDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate() - 1,
+  );
+  return getPreviousStartOfTheWeek(prevDay, startOfTheWeek);
+};
+
 export const Calendar = ({ date }: CalendarProps) => {
   const rows = 10;
   const renderedRows: Array<React.JSX.Element>[] = [];
+
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  const startDate = getPreviousStartOfTheWeek(date, weekStartDay);
+
   for (let i = 0; i < rows; i++) {
     renderedRows.push([]);
   }
   renderedRows.forEach((row, rowIndex) => {
     for (let day = 0; day < 7; day++) {
       const cellDate = new Date(
-        date.getFullYear(),
-        date.getMonth(),
-        date.getDate() + rowIndex * 7 + day,
+        startDate.getFullYear(),
+        startDate.getMonth(),
+        startDate.getDate() + rowIndex * 7 + day,
       );
       row.push(
-        <CalendarCell key={"" + rowIndex + "-" + date} date={cellDate} />,
+        <CalendarCell key={"" + rowIndex + "-" + day} date={cellDate} />,
       );
     }
   });
