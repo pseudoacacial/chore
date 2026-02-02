caf780d598295f287465eebe72cd541ec2f2af55
pseudoacacial <aibanzai@protonmail.com>
2026-02-02 23:20:03 +0100

    isToday, isEvenMonth

--- app/components/ui/calendarCell.tsx
+++ app/components/ui/calendarCell.tsx
@@ -5,8 +5,13 @@ type CalendarCellProps = {
 };
 
 export const CalendarCell = ({ date }: CalendarCellProps) => {
+  const today = new Date();
+  const isToday = date.toDateString() === today.toDateString();
+  const isEvenMonth = date.getMonth() % 2;
   return (
-    <div className="flex justify-center flex-1 w-8 border">
+    <div
+      className={`flex justify-center flex-grow w-8 box-border  border ${isToday ? "border-red-600 border-2" : "border-slate-700"} ${isEvenMonth ? "bg-slate-100" : "bg-slate-200"}`}
+    >
       {date.getDate()}
     </div>
   );
