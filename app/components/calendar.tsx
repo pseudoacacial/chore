8c65a394aef0c39c06b940773d9866c385c6435b
pseudoacacial <aibanzai@protonmail.com>
2026-02-03 22:34:45 +0100

    scrolling top and bottom

--- app/components/calendar.tsx
+++ app/components/calendar.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CalendarCell from "./ui/calendarCell";
 import CalendarRow from "./ui/calendarRow";
-import { Text } from "react-native";
+import { View, VirtualizedList } from "react-native";
 
 type CalendarProps = {
   date: Date;
@@ -22,40 +22,82 @@ const getPreviousStartOfTheWeek = (date: Date, startOfTheWeek = 0) => {
 };
 
 const Calendar = ({ date }: CalendarProps) => {
-  const rows = 100;
-  const renderedRows: Array<Date>[] = [];
-
   const month = date.getMonth();
   const year = date.getFullYear();
+  const initialIndex = useRef(100);
+  const virtualizedListRef = useRef(null);
+
+  const [data, setData] = useState<Array<Date[]>>([]);
 
   const startDate = getPreviousStartOfTheWeek(date, weekStartDay);
 
-  for (let i = 0; i < rows; i++) {
-    renderedRows.push([]);
-  }
-  renderedRows.forEach((row, rowIndex) => {
+  const generateWeek = (startDate: Date) => {
+    const row: Date[] = [];
     for (let day = 0; day < 7; day++) {
       const cellDate = new Date(
         startDate.getFullYear(),
         startDate.getMonth(),
-        startDate.getDate() + rowIndex * 7 + day,
+        startDate.getDate() + day,
       );
       row.push(cellDate);
     }
-  });
+    return row;
+  };
 
+  const generateInitialDates = () => {
+    const rows: Array<Date>[] = [];
+
+    // generate rows before today
+    for (let i = 100; i > 0; i--) {
+      const cellDate = new Date(startDate);
+      cellDate.setDate(cellDate.getDate() - i * 7);
+      rows.push(generateWeek(cellDate));
+    }
+
+    // add this week
+    const cellDate = new Date(startDate);
+    rows.push(generateWeek(cellDate));
+
+    // generate rows after today
+    for (let i = 1; i <= 100; i++) {
+      const cellDate = new Date(startDate);
+      cellDate.setDate(cellDate.getDate() + i * 7);
+      rows.push(generateWeek(cellDate));
+    }
+    return rows;
+  };
+
+  useEffect(() => {
+    setData(generateInitialDates());
+  }, []);
+
+  const getItem = (_data: unknown, index: number): Date[] => data[index];
+
+  const getItemCount = (_data: unknown) => data.length;
   return (
-    <div className="h-[1000px] overflow-scroll">
-      <div className="relative">
-        {renderedRows.map((row, i) => (
-          <CalendarRow key={row[0].toDateString()}>
-            {row.map((cellDate) => (
+    <View className="flex-1 h-full w-[240px]">
+      <VirtualizedList
+        className="flex-1 web:h-screen"
+        ref={virtualizedListRef}
+        initialNumToRender={200}
+        data={data}
+        renderItem={({ item }) => (
+          <CalendarRow key={item[0].toDateString()}>
+            {item.map((cellDate) => (
               <CalendarCell key={cellDate.toDateString()} date={cellDate} />
             ))}
           </CalendarRow>
-        ))}
-      </div>
-    </div>
+        )}
+        getItem={getItem}
+        getItemCount={getItemCount}
+        initialScrollIndex={initialIndex.current}
+        getItemLayout={(_data, index) => ({
+          length: 24,
+          offset: 24 * index,
+          index: index,
+        })}
+      ></VirtualizedList>
+    </View>
   );
 };
 
--- app/components/ui/calendarCell.tsx
+++ app/components/ui/calendarCell.tsx
@@ -10,7 +10,7 @@ const CalendarCell = ({ date }: CalendarCellProps) => {
   const isEvenMonth = date.getMonth() % 2;
   return (
     <div
-      className={`flex justify-center flex-grow w-8 box-border  border ${isToday ? "border-red-600 border-2" : "border-slate-700"} ${isEvenMonth ? "bg-slate-100" : "bg-slate-200"}`}
+      className={`flex justify-center flex-grow w-8 h-6 box-border  border ${isToday ? "border-red-600 border-2" : "border-slate-700"} ${isEvenMonth ? "bg-slate-100" : "bg-slate-200"}`}
     >
       {date.getDate()}
     </div>
